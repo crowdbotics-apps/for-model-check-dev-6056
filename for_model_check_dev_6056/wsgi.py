91b221eedf440725269242a07846034d2f08ec0f
Crowdbotics <team@crowdbotics.com>
2020-06-15 16:15:43 +0000

    Update models

--- for_model_check_dev_6056/wsgi.py
+++ for_model_check_dev_6056/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'for_model_check_dev_6056.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "for_model_check_dev_6056.settings")
 
 application = get_wsgi_application()
--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -1,3 +1,6 @@
+from rest_framework import viewsets
+from rest_framework import authentication
+from .serializers import ChecSerializer, CustomTextSerializer, HomePageSerializer
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import IsAdminUser
@@ -11,7 +14,7 @@ from home.api.v1.serializers import (
     HomePageSerializer,
     UserSerializer,
 )
-from home.models import CustomText, HomePage
+from home.models import Chec, CustomText, HomePage
 
 
 class SignupViewSet(ModelViewSet):
@@ -49,3 +52,12 @@ class HomePageViewSet(ModelViewSet):
     authentication_classes = (SessionAuthentication, TokenAuthentication)
     permission_classes = [IsAdminUser]
     http_method_names = ["get", "put", "patch"]
+
+
+class ChecViewSet(viewsets.ModelViewSet):
+    serializer_class = ChecSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Chec.objects.all()
